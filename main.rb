ec231f150b55105832602f7a4a9a2e7e55715756
Stefano <snebo54@hotmail.com>
2023-12-26 23:28:25 +0100

    Style and formatting changes

--- main.rb
+++ main.rb
@@ -1,27 +1,5 @@
 # frozen_string_literal: true
 
-# Handle player information
-# class Player
-#   attr_accessor :name, :type
-
-#   # probably have maker and breaker methods so i can switch turns
-#   # have an is_guesser
-# end
-
-# # Handles game logic
-# class Game
-#   @@slots = 4
-#   def set_slots
-#     # changes amount of guessing slots for the game
-#     response = ''
-#     until Array(3..9).include?(response.to_i)
-#       print 'Enter the number of guessing slots -> '
-#       response = gets.chomp
-#     end
-#     @@slots = response.to_i
-#   end
-# end
-
 COLORS = %w[red blue yellow black green]
 $guessed = false
 
@@ -36,15 +14,14 @@ end
 
 def guess(m_guess, slots)
   puts 'Enter the colors in order, one by one'
-  
-  until $guessed # change this to 'guessed?''
-    my_guess = [] # making sure its a new guess each time
+
+  until $guessed 
+    my_guess = []
     # making sure a valid color is guessed
-    puts slots
-    slots.times do |i|
+    slots.times do
       val = ''
       until COLORS.include?(val)
-        print "\nGuess a valid color -> "
+        print 'Guess a valid color -> '
         val = gets.chomp.downcase
       end
       my_guess.push(val)
@@ -56,7 +33,6 @@ def guess(m_guess, slots)
 end
 
 def check_guess(my_g, m_g, slots)
-  puts 'does this run'
   result = []
   dummy_guess = m_g.clone
   puts "checking #{my_g} vs #{m_g}"
@@ -78,7 +54,6 @@ def check_guess(my_g, m_g, slots)
       idx = dummy_guess.find_index(my_g[i])
       dummy_guess[idx] = nil
       result.push('white')
-      # pop the first match
     end
   end
   puts "result: #{result.join(', ')}"
@@ -87,11 +62,11 @@ end
 slots = 0
 m_guess = []
 until Array(3..9).include?(slots)
-  puts 'enter no of slots -> '
+  print 'enter no of slots -> '
   slots = gets.chomp.to_i
 end
+# One round
 puts "Choose from these colors: #{COLORS.join(', ').upcase}"
 slots.times { |i| make_guess(m_guess, i + 1) }
 puts "Makers guess: #{m_guess.inspect}" # to be hidden
-
 guess(m_guess, slots)
